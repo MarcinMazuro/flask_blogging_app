5c3088b2dbc177e801f2efa62fcdd0d968395e1c
Marcin <mazuromarcin@gmail.com>
2025-09-21 00:35:12 +0200

    add moment and display time

--- hello.py
+++ hello.py
@@ -1,12 +1,15 @@
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from datetime import datetime, timezone
 
 app = Flask(__name__)
 Bootstrap(app)
+Moment(app)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', current_time=datetime.now(timezone.utc))
 
 @app.route('/user/<name>')
 def user(name):
