6cfe6a3d6283e7bf5a7af3152c75c5a519db5a22
Marcin <mazuromarcin@gmail.com>
2025-09-22 00:53:15 +0200

    add shell integration

--- hello.py
+++ hello.py
@@ -45,6 +45,11 @@ class User(db.Model):
 
     def __repr__(self):
         return f'<User {self.username}>'
+    
+
+@app.shell_context_processor
+def make_shell_context():
+    return dict(db=db, User=User, Role=Role)
 
 
 @app.route('/', methods=['GET', 'POST'])
